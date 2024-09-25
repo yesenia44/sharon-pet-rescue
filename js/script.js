f70fd3d67aaef352fc6267aa4451cfb7f1e2a2db
yesenia_v <yeseniavsqz@gmail.com>
2024-09-25 10:54:49 -0700

    Created an array, a loop and a click event to display the objects as a list in the browser.

--- js/script.js
+++ js/script.js
@@ -13,15 +13,15 @@ const createPet = function (name, species) {
 
         play: function() {
             if (this.isTired === 10) { // if this.isTired equals to 10
-            console.log("Too tired to play.");// then pet is tired to play
-            this.sleep(); // calls sleep function. 
+                console.log("Too tired to play.");// then pet is tired to play
+                this.sleep(); // calls sleep function. 
             } else {   
             console.log(`Yay ${this.name} loves to play!`)
             this.isTired += 1// increase the value by 1.
             }
         }
     }
-    return pet;
+    return pet; 
 };
 
 const sora = createPet("Sora", "ferret");
@@ -36,6 +36,35 @@ clover.sleep(); //value for isTired went from 5 to 1 because the function was ca
 baxter.play(); // value for isTired went from 5 to 6 because it adds 1 after being called
 
 console.log(clover, baxter);
+
+clover.isTired = 8; // redefine isTired property to 8
+francine.isTired = 9; //redefine isTired property to 9
+
+const allPets = [sora, clover, baxter, cleo, francine]; // array 
+console.log(allPets);
    
+const showPets = function (petArray) { // function with petArray as a parameter
+    pets.innerHTML = ""; //empty string. empties the list when showPets runs
+    for (let pet of petArray) { // for .. of loop
+        let status = "ready to play"; // declaring a variable
+        if(pet.isTired >= 7) { // if statement. if pet.isTired is greater or equal to 7
+            status = "sleeping"; // pet is sleeping
+        }
+        const li = document.createElement("li");  // creating a list element
+        li.innerHTML = `<span class= "pet-name">${pet.name}</span> the ${pet.species} is ${status}`; // add span class which adds color on pet name
+        //add pet, object, before adding the property so it won't show up as undefined.
+        pets.append(li) // adds list to pets
+    }
+}
+statusButton.addEventListener("click", function () { // when clicking the button, the function will occur
+    showPets(allPets)// calls the function with allPets as parameter
+    }
+);
+    
+       
+        
+        
+       
+    
 
 
